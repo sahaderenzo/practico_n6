ed2fb240f719311e33a110e5627921af1fd3a7fb
SahadeRenzo <sahaderenzo02@gmail.com>
2024-09-17 12:57:09 -0300

    Fin del cuarto video

---
+++ src/main/java/com/example/inicial1/services/BaseService.java
@@ -0,0 +1,12 @@
+package com.example.inicial1.services;
+
+import java.rmi.server.ExportException;
+import java.util.List;
+
+public interface BaseService<E> {
+    public List<E> findAll() throws Exception;
+    public E findById(Long id) throws Exception;
+    public E save(E entity) throws Exception;
+    public E update (Long id, E entity) throws Exception;
+    public boolean delete(Long id) throws Exception;
+}
--- src/main/java/com/example/inicial1/services/PersonaServices.java
+++ src/main/java/com/example/inicial1/services/PersonaServices.java
@@ -1,10 +1,35 @@
 package com.example.inicial1.services;
 
+import com.example.inicial1.entities.Persona;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class PersonaServices {
+public class PersonaServices implements BaseService<Persona>{
 
+    @Override
+    public List<Persona> findAll() throws Exception {
+        return List.of();
+    }
 
+    @Override
+    public Persona findById(Long id) throws Exception {
+        return null;
+    }
 
+    @Override
+    public Persona save(Persona entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona update(Long id, Persona entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
 }
